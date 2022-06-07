6218b2355f50d8a4ed1ad6f2efca36add8a6170e
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2022-06-07 13:15:17 +0530

    Updated Cardmodel

--- app/src/main/java/com/example/smartattendance/model/CardModel.kt
+++ app/src/main/java/com/example/smartattendance/model/CardModel.kt
@@ -1,28 +1,8 @@
-package com.example.smartattendance.model;
+package com.example.smartattendance.model
 
-public class CardModel {
-    private String  name, Class,  image;
-    private  int roll;
-    public CardModel(String name, int roll, String Class, String image){
-        this.name = name;
-        this.roll = roll;
-        this.Class = Class;
-        this.image = image;
-    }
-
-    public String getMyClass() {
-        return Class;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public int getRoll() {
-        return roll;
-    }
-}
+class CardModel(
+    val name: String? = null,
+    val roll: Int? = null,
+    val sem: String? = null,
+    val image: String? = null
+    )
